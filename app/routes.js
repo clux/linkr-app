3adb224e49edbfd910f5f309ca24eed7eb50f3f7
clux <analsandblaster@gmail.com>
2015-03-12 19:10:51 +0000

    last express commit

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,7 @@ var isLoggedIn = function (req, res, next) {
 
 module.exports = function (app, passport) {
   // main
-  app.get('/', function(req, res) {
+  app.get('/', function (req, res) {
     res.render('index.ejs', { user: req.user });
   });
 
@@ -20,12 +20,15 @@ module.exports = function (app, passport) {
     successRedirect : '/profile', // redirect to the secure profile section
     failureRedirect : '/login', // redirect to signup on error
   }));
+
   // TODO: get messages in there!
+  // currently they are lost because we redirect
+  // would have to use connect-flash or AJAX to get them..
   app.get('/signup', function (req, res) {
-    res.render('signup.ejs', { message: "woot", user: req.user });
+    res.render('signup.ejs', { user: req.user, message: '' });
   });
   app.get('/login', function (req, res) {
-    res.render('login.ejs', { message: "hoot", user: req.user });
+    res.render('login.ejs', { user: req.user, message: '' });
   });
   app.get('/logout', function (req, res) {
     req.logout();
