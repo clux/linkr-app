15d0b21b726e82e54b02e52470f5bb5710554377
clux <analsandblaster@gmail.com>
2015-06-11 23:16:56 +0100

    test web component build system

---
+++ test/build.test.js
@@ -0,0 +1,16 @@
+var test = require('bandage');
+var fs = require('co-fs');
+
+test('web components', function *(t) {
+  var html = yield fs.readFile('./assets/html/components.html', 'utf8');
+  var pmr = 'Polymer = {'; // a string that's unique in polymer.html
+  t.ok(html.indexOf(pmr) > 0, 'polymer object found');
+  t.equal(html.indexOf(pmr), html.lastIndexOf(pmr), 'polymer not found twice');
+
+  t.ok(html.indexOf('ice-breaker') >= 0, 'ice-breaker found');
+  t.ok(html.indexOf('linkr-list') >= 0, 'linkr-list found');
+
+  t.ok(html.indexOf('src="') < 0, 'no external src links');
+  t.ok(html.indexOf('data:image/png;base64') >= 0, 'images inlined');
+  // could maybe enforce a max size of inlined images here
+});
