2cb4ea8a7c3696b1ecd633e6bba46e29c7cef9c1
clux <analsandblaster@gmail.com>
2015-08-17 22:16:13 +0100

    basic build script improvements

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ set -eu
 
 PATH="$(npm bin):${PATH}"
 VER=$(git log | head -1 | cut -d " " -f 2)
-DEV=1
+NENV=${NODE_ENV-development}
 
-assets() {
+linkr_assets() { #HELP Generate assets:\nLINKR assets
   mkdir -p assets/js assets/css assets/images assets/html
   cp bower_components/webcomponentsjs/webcomponents-lite.* assets/js
   cp bower_components/platinum-sw/service-worker.js assets/js
@@ -14,15 +14,28 @@ assets() {
   vulcanize app/elements.html > assets/html/components.html
 }
 
-cache () {
+linkr_cache () { #HELP Generate cache-config.json based on NODE_ENV:\nLINKR cache
   # all asset files, wrap in quotes with trailing comma, then add final "./"
-  local cached=$(find assets/ -type f | sed 's/assets\///' | sed 's/\(.*\)/"\1",/g')
+  local cached
+  cached=$(find assets/ -type f | sed 's/assets\///' | sed 's/\(.*\)/"\1",/g')
   local manifest="${cached} \"./\""
   echo '{ "cacheId": "linkr-app" }' \
-     | json -e this.disabled=$([ $DEV -eq 1 ] && echo "true" || echo "false") \
+     | json -e this.disabled="$([ "$NENV" = "development" ] && echo true || echo false)" \
      | json -e "this.precache=[${manifest}]" \
-     | json -e "this.precacheFingerprint=\"${VER}\"" # > assets/cache-config.json
+     | json -e "this.precacheFingerprint=\"${VER}\"" > assets/cache-config.json
 }
 
-assets
-cache
+linkr_all() { #HELP Generate everything:\nLINKR
+  linkr_assets
+  linkr_cache
+}
+
+linkr_help() {
+  sed -n "s/^.*#HELP\\s//p;" < "$1" | sed "s/\\\\n/\n\t/g;s/$/\n/;s!LINKR!${1/!/\\!}!g"
+}
+
+[[ -z "${1-}" ]] && linkr_all && exit 0
+case $1 in
+  assets|cache) linkr_"$1" "${@:2}" ;;
+  *) linkr_help "$0" ;;
+esac
--- development.sh
+++ development.sh
@@ -4,3 +4,4 @@ export DATABASE_URL=postgres://$USER:$DATABASE_PASSWORD@localhost:5432/linkr
 export LINKR_USER=clux
 export LINKR_EMAIL=h@x.io
 export LINKR_PASSWORD=heythere
+export NODE_ENV=development
