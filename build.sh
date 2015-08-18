a8b22c8ddb0b7af9269403069c9ce5440f620cc7
clux <analsandblaster@gmail.com>
2015-08-18 19:03:52 +0100

    disable service worker elements for now - a bit too unstable - but some of the logic for it is prepared in the build script at least

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ linkr_assets() { #HELP Generate assets:\nLINKR assets
   mkdir -p assets/js assets/css assets/images assets/html
   cp bower_components/webcomponentsjs/webcomponents-lite.* assets/js
   cp bower_components/platinum-sw/service-worker.js assets/js
+  # TODO: shouldn't this be in main after an event rather?
+  echo "importScripts('js/service-worker.js');" > assets/sw-import.js
   browserify app/main.js > assets/js/main.js
   uglifyjs assets/js/main.js > assets/js/main.min.js
   vulcanize app/elements.html > assets/html/components.html
@@ -27,7 +29,7 @@ linkr_cache () { #HELP Generate cache-config.json based on NODE_ENV:\nLINKR cach
 
 linkr_all() { #HELP Generate everything:\nLINKR
   linkr_assets
-  linkr_cache
+  #linkr_cache
 }
 
 linkr_help() {
