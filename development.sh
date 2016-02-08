8030e4b45136468ff920c1c967a4c77651ef5d86
clux <analsandblaster@gmail.com>
2016-02-08 22:29:51 +0000

    simpler database setup locally

--- development.sh
+++ development.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-export DATABASE_PASSWORD=arst
-export DATABASE_URL=postgres://$USER:$DATABASE_PASSWORD@localhost:5432/linkr
+export DATABASE_URL=postgres://$USER@localhost:5432/linkr
 export LINKR_USER=clux
 export LINKR_EMAIL=h@x.io
 export LINKR_PASSWORD=heythere
